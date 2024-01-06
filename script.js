c5e813d910948ed124914bd9273a7517c4cf1b11
Ebenmars <76241509+Ebenmars@users.noreply.github.com>
2024-01-05 20:00:49 -0500

    Done

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+
+const cards = document.querySelectorAll(".card");
+
+
+cards.forEach((card) => {
+    card.addEventListener("click",  () =>  {
+      removeActiveClass();
+      card.classList.add("active");
+    });
+})
+
+
+function removeActiveClass() {
+  cards.forEach((card) => {
+    card.classList.remove("active");
+  })
+}
+
